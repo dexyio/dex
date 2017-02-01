d5ed52b84b06a5f1db93db1f3087f6cd6d184436
Kook Maeng <ykmaeng@gmail.com>
2017-02-01 19:54:07 +0900

    Add indecies int, long, double and string

--- src/yz_dexyml_extractor.erl
+++ src/yz_dexyml_extractor.erl
@@ -6,26 +6,27 @@ extract(Val) ->
     extract(binary_to_term(Val), []).
 
 extract(Props, _Opts) when is_list(Props) ->
-	error_logger:info_msg("yz_dexyml_extractor:extract/2 -> Props: ~p~n", [Props]),
+	%error_logger:info_msg("yz_dexyml_extractor:extract/2 -> Props: ~p~n", [Props]),
 	lists:flatten(fields(Props, []));
 extract(_, _) ->
 	[].
 
 fields([], Acc) ->
-	error_logger:info_msg("yz_dexyml_extractor:fields/2 -> Acc: ~p~n", [Acc]),
+	%error_logger:info_msg("yz_dexyml_extractor:fields/2 -> Acc: ~p~n", [Acc]),
 	Acc;
 fields([{bucket, V} = H | T], Acc) when is_bitstring(V) -> fields(T, [H | Acc]);
 fields([{key, V} = H | T], Acc) when is_bitstring(V) -> fields(T, [H | Acc]);
-fields([{value, V} = H | T], Acc) when is_bitstring(V) -> fields(T, [H | Acc]);
-%fields([{datetime, V} = H | T], Acc) when is_bitstring(V) -> fields(T, [H | Acc]);
-fields([{created, Usecs} | T], Acc) when is_number(Usecs) ->
-	%Secs = trunc(Usecs / 1000000),
-	fields(T, [{created, Usecs} | Acc]);
+fields([{created, Usecs} | T], Acc) when is_number(Usecs) -> fields(T, [{created, Usecs} | Acc]);
 fields([{tags, V} | T], Acc) when is_list(V) ->
 	case tags(V, []) of
 		[] -> fields(T, Acc);
 		Tags -> fields(T, [Tags | Acc])
 	end;
+fields([{idx_int, V} = H | T], Acc) when is_number(V) -> fields(T, [H | Acc]);
+fields([{idx_long, V} = H | T], Acc) when is_number(V) -> fields(T, [H | Acc]);
+fields([{idx_float, V} = H | T], Acc) when is_number(V) -> fields(T, [H | Acc]);
+fields([{idx_double, V} = H | T], Acc) when is_number(V) -> fields(T, [H | Acc]);
+fields([{idx_string, V} = H | T], Acc) when is_bitstring(V) -> fields(T, [H | Acc]);
 fields([_ | T], Acc) -> fields(T, Acc).
 
 tags([], Acc) -> Acc;
